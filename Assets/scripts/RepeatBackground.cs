3646d903e1c0598665f90efc36ef60f81bddba03
Robin van Meeuwen <328912@student.mboutrecht.nl>
2019-12-20 19:12:10 +0100

    no message

---
+++ Assets/scripts/RepeatBackground.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackground : MonoBehaviour
+{
+    private Vector3 startPos;
+    private float repeatWitdth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // achtergrond oneiding latene worden
+        startPos = transform.position;
+        repeatWitdth = GetComponent<BoxCollider>().size.x / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < startPos.x -repeatWitdth)
+        { 
+            transform.position = startPos;
+            { 
+                transform.position = startPos;
+            }
+        }
+    }
+}
